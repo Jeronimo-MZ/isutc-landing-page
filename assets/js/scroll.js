ec54312935e3b3c0eb994cc97bdc0876afdf077f
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-02-11 01:18:00 +0200

    :package: section#banner (slider)

---
+++ assets/js/scroll.js
@@ -0,0 +1,43 @@
+var scrollWidth = 0;
+const delay = ms => new Promise(res => setTimeout(res, ms));
+let numImages = document.querySelectorAll('#banner .container img').length;
+
+async function scroll (direction) {
+    let value;
+    if (direction === 'left') {
+        value = 5;
+    } else if (direction === 'right') {
+        value = -5;
+    } else {
+        return;
+    }
+    let imgContainer = document.querySelector('#banner .container');
+    for (let i = 0; i < 20; i++) {
+        await delay(5);
+        scrollWidth += value;
+        imgContainer.style.marginLeft = scrollWidth+'vw';
+    }
+}
+
+    const btnPrevious = document.getElementById('btn-previous');
+    const btnNext = document.getElementById('btn-next');
+
+    btnPrevious.addEventListener('click', () => {
+        if ((scrollWidth+1) > 0) {
+            for (let i = 1; i < numImages; i++) {
+                scroll('right');
+            }
+        } else {
+
+            scroll('left');
+        }
+    });
+    btnNext.addEventListener('click', () => {
+        if ((-scrollWidth/100) < numImages-1) {
+            scroll('right')
+        } else {
+            for (let i = 1; i < numImages; i++) {
+                scroll('left');
+            }
+        }
+    });
